a43803db8dccd969cefc4e2ea0de96a8217f6e7d
Edward Iga Kigongo <igakigongo@gmail.com>
2019-05-06 16:01:16 +0200

    Lists

---
+++ Lists.rb
@@ -0,0 +1,38 @@
+class Node
+ attr_accessor :value, :next_node
+
+ def initialize(value, next_node = nil)
+  @value = value
+  @next_node = next_node
+ end
+end
+
+class LinkedList
+ # setup head and tail
+ attr_accessor :head, :tail
+
+ def add(number)
+  $node = Node.new(number)
+  if @head.nil?
+   @head = @tail = $node
+  else
+   @tail.next_node = $node
+   @tail = $node
+  end
+ end
+
+ def get(index)
+  $current = @head
+  while index > 0 && $current
+   $current = $current.next_node
+   index -= 1
+  end
+  $current.value
+ end
+end
+
+list = LinkedList.new
+
+list.add(3)
+list.add(5)
+puts list.get(1)
