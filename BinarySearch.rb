b1be65b2c95a08501f96cfee8a73eb9f42300ce3
Edward Iga Kigongo <igakigongo@gmail.com>
2019-05-15 12:14:15 +0200

    Binary Search

---
+++ BinarySearch.rb
@@ -0,0 +1,12 @@
+def sqrt(number)
+  sqrt_recursive(number, 0, number)
+end
+
+def sqrt_recursive(number, s, e)
+  m = ((e - s) / 2) + s
+  x = m ** 2
+  return x == number ? m : x < number ? sqrt_recursive(number, m + 1, e) : sqrt_recursive(number, s, m - 1)
+end
+
+puts sqrt(25)
+puts sqrt(7056)
