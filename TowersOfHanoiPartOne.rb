272630ba1b111bac357717aff214054fd07cfc64
Edward Iga Kigongo <igakigongo@gmail.com>
2019-05-15 12:06:16 +0200

    Towers of Hanoi - Restricted to 2 discs (the base case)

---
+++ TowersOfHanoiPartOne.rb
@@ -0,0 +1,10 @@
+def move(s, g)
+  aux = (1..3).find { |d| d != s && d != g }
+  "#{s}->#{aux} #{s}->#{g} #{aux}->#{g}"
+end
+
+puts move(1, 3)
+# => 1->2 1->3 2->3
+
+puts move(2, 3)
+# => 2->1 2->3 1->3
