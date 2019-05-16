da4a3f9861ce3292aba54cc586556c3417051226
Edward Iga Kigongo <igakigongo@gmail.com>
2019-05-16 11:08:12 +0200

    Towers of Hanoi Part II

---
+++ TowersOfHanoiPartTwo.rb
@@ -0,0 +1,18 @@
+def hanoi_steps(number_of_discs)
+  tower(number_of_discs, 1, 3, 2)
+end
+
+def tower(n, from, to, aux)
+  if n == 1
+    puts "#{from}->#{to}"
+    return
+  end
+
+  tower(n - 1, from, aux, to)
+  puts "#{from}->#{to}"
+  tower(n - 1, aux, to, from)
+end
+
+#hanoi_steps(2)
+#hanoi_steps(3)
+#hanoi_steps(4)
